67879cff4a6472a08ef70ee897db2e5c858f7695
wangmeng <wangmeng@bbdservice.com>
2017-08-01 11:41:10 +0800

    js编译

--- demo/progress.js
+++ demo/progress.js
@@ -1,36 +1,38 @@
+'use strict';
+
 /**
  * Created by Administrator on 2017/7/31.
  */
 
-(function(global){
+(function (global) {
     var tool = {
-        init: function(elem,option,funName){
-            for(var key in option){
+        init: function init(elem, option, funName) {
+            for (var key in option) {
                 var target = elem[funName];
-                if(typeof target === 'function'){
-                    var param = [key,option[key]];
-                    for(var i=0,l=target.length-2; i<l; i++){
+                if (typeof target === 'function') {
+                    var param = [key, option[key]];
+                    for (var i = 0, l = target.length - 2; i < l; i++) {
                         param.unshift(null);
                     }
-                    target.apply(elem,param);
-                }else{
-                    target[key] = option[key]
+                    target.apply(elem, param);
+                } else {
+                    target[key] = option[key];
                 }
             }
         },
-        attr: function(elem,option){
-            this.init(elem,option,'setAttribute');
+        attr: function attr(elem, option) {
+            this.init(elem, option, 'setAttribute');
         },
-        attrNS: function(elem,option){
-            this.init(elem,option,'setAttributeNS');
+        attrNS: function attrNS(elem, option) {
+            this.init(elem, option, 'setAttributeNS');
         },
-        css: function(elem,option){
-            this.init(elem,option,'style');
-        },
+        css: function css(elem, option) {
+            this.init(elem, option, 'style');
+        }
     };
 
     // svg生成函数
-    function ProgressCreate(options){
+    function ProgressCreate(options) {
         var defaults = {
             type: 'circle',
             percent: 0
@@ -46,48 +48,52 @@
         var svgns = "http://www.w3.org/2000/svg";
 
         var viewBox = 100; // 可视区
-        var center = viewBox/2; // 中心位置（圆心位置）
+        var center = viewBox / 2; // 中心位置（圆心位置）
         var strokeWidth = 6; // 线宽
-        var Rr = center-strokeWidth/2; // 圆真实的半径
-        var c = (2*Math.PI* Rr).toFixed(2); // 圆周长
+        var Rr = center - strokeWidth / 2; // 圆真实的半径
+        var c = (2 * Math.PI * Rr).toFixed(2); // 圆周长
 
         // svg容器
-        var svgBox= document.createElement('div');
+        var svgBox = document.createElement('div');
         tool.attr(svgBox, { class: 'progress-inner' });
-        tool.css(svgBox, { width: '132px',height: '132px','font-size': '20px' });
+        tool.css(svgBox, { width: '132px', height: '132px', 'font-size': '20px' });
 
         // svg
-        var svg = document.createElementNS(svgns,'svg');
-        tool.attrNS(svg,{ viewBox: `0 0 ${viewBox} ${viewBox}` });
+        var svg = document.createElementNS(svgns, 'svg');
+        tool.attrNS(svg, { viewBox: '0 0 ' + viewBox + ' ' + viewBox });
 
         // baseAttr & baseStyle
-        var startY = -Rr,circleStartY= 2*Rr,circleEndY = -2*Rr,dasharray= `${c}px 1000px`,dashoffset = 0,fullPercent = c;
-        if(type === 'dashboard'){
-            startY = Rr,circleStartY= -2*Rr,circleEndY = 2*Rr,dasharray= `${c/4*3}px 1000px`,dashoffset = `-${c/4/2}px`,fullPercent = c/4*3;
+        var startY = -Rr,
+            circleStartY = 2 * Rr,
+            circleEndY = -2 * Rr,
+            dasharray = c + 'px 1000px',
+            dashoffset = 0,
+            fullPercent = c;
+        if (type === 'dashboard') {
+            startY = Rr, circleStartY = -2 * Rr, circleEndY = 2 * Rr, dasharray = c / 4 * 3 + 'px 1000px', dashoffset = '-' + c / 4 / 2 + 'px', fullPercent = c / 4 * 3;
         }
-        var basePathAttr = {d: `M ${center},${center} m 0,${startY} a ${Rr},${Rr} 0 1 1 0,${circleStartY} a ${Rr},${Rr} 0 1 1 0,${circleEndY}`, 'stroke-width': strokeWidth, 'fill-opacity': '0', 'stroke-linecap': "round" };
-        var baseStyle = {'stroke-dasharray': dasharray, 'stroke-dashoffset': dashoffset, transition: 'stroke-dashoffset 0.3s ease 0s, stroke-dasharray 0.3s ease 0s, stroke 0.3s'};
-
+        var basePathAttr = { d: 'M ' + center + ',' + center + ' m 0,' + startY + ' a ' + Rr + ',' + Rr + ' 0 1 1 0,' + circleStartY + ' a ' + Rr + ',' + Rr + ' 0 1 1 0,' + circleEndY, 'stroke-width': strokeWidth, 'fill-opacity': '0', 'stroke-linecap': "round" };
+        var baseStyle = { 'stroke-dasharray': dasharray, 'stroke-dashoffset': dashoffset, transition: 'stroke-dashoffset 0.3s ease 0s, stroke-dasharray 0.3s ease 0s, stroke 0.3s' };
 
         // pathTrail
-        var pathTrail = document.createElementNS(svgns,'path');
-        tool.attr(pathTrail,{ class: 'progress-circle-trail'});
-        tool.attrNS(pathTrail,basePathAttr);
-        tool.css(pathTrail,baseStyle);
+        var pathTrail = document.createElementNS(svgns, 'path');
+        tool.attr(pathTrail, { class: 'progress-circle-trail' });
+        tool.attrNS(pathTrail, basePathAttr);
+        tool.css(pathTrail, baseStyle);
 
         // pathCircle
-        var pathCircle = document.createElementNS(svgns,'path');
-        if(percent>0){
-            baseStyle['stroke-dasharray'] = `${percent/100 * fullPercent}px 1000px`;   // 计算百分比所占的长度
-            tool.attr(pathCircle,{ class: 'progress-circle-path'});
-            tool.attrNS(pathCircle,basePathAttr);
-            tool.css(pathCircle,baseStyle);
+        var pathCircle = document.createElementNS(svgns, 'path');
+        if (percent > 0) {
+            baseStyle['stroke-dasharray'] = percent / 100 * fullPercent + 'px 1000px'; // 计算百分比所占的长度
+            tool.attr(pathCircle, { class: 'progress-circle-path' });
+            tool.attrNS(pathCircle, basePathAttr);
+            tool.css(pathCircle, baseStyle);
         }
 
         // percentText
         var percentText = document.createElement('span');
-        tool.attr(percentText,{ class: 'progress-text'});
-        percentText.innerHTML = options.percentText || `${percent}%`;
+        tool.attr(percentText, { class: 'progress-text' });
+        percentText.innerHTML = options.percentText || percent + '%';
 
         // render
         svg.appendChild(pathTrail);
@@ -100,11 +106,11 @@
 
     // 对外暴露函数
     var Progress = {};
-    Progress.circle = function(percent,percentText){
-        return ProgressCreate({ type: 'circle',percent: percent,percentText: percentText });
+    Progress.circle = function (percent, percentText) {
+        return ProgressCreate({ type: 'circle', percent: percent, percentText: percentText });
     };
-    Progress.dashboard = function(percent,percentText){
-        return ProgressCreate({ type: 'dashboard',percent: percent,percentText: percentText });
+    Progress.dashboard = function (percent, percentText) {
+        return ProgressCreate({ type: 'dashboard', percent: percent, percentText: percentText });
     };
     global.Progress = Progress;
 })(window);
